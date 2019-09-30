9da452ad51257b9886051df8f31a3c616c993010
mertbilgic <34304850+mertbilgic@users.noreply.github.com>
2019-09-30 21:48:01 +0300

    first commit

---
+++ BuildingForms/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingForms.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+        
+    }
+}
